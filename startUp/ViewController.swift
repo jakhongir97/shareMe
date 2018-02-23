f76eecda81b5e448418d6c821bf3196b01d5c57a
Jakhongir Nematov <jahongirnematov@jahongirs-macbook-pro.local>
2018-02-23 12:23:10 +0500

    Initial Commit

---
+++ startUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  startUp
+//
+//  Created by Jahongir Nematov on 2/23/18.
+//  Copyright © 2018 Jahongir Nematov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
